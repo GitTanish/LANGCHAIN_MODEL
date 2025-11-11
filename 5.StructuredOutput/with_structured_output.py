8a7d99d198e98a2b8236db3b2450ed6b52cf9fe9
GitTanish <tanishsaroj6781@gmail.com>
2025-11-11 21:42:15 +0530

    pydantic

--- 5.StructuredOutput/with_structured_output.py
+++ 5.StructuredOutput/with_structured_output.py
@@ -1,6 +1,6 @@
 from langchain_groq import ChatGroq
 from dotenv import load_dotenv
-from typing import TypedDict, Annotated
+from typing import TypedDict, Annotated, Optional
 
 load_dotenv()
 
@@ -12,6 +12,8 @@ class Review(TypedDict):
     key_themes: Annotated[list[str], "Write down all the key themes discussed in the review list"]
     summary: Annotated[str, "write a summary within 100 words"]
     sentiment: Annotated[str, "Return Sentiment of the review either negative positive or neutral"]
+    pros: Annotated[Optional[list[str]], "Write down all the postitive points"]
+    cons: Annotated[Optional[list[str]], "Write down all the negative points"]
 
 structured_model =model.with_structured_output(Review)
 
@@ -25,6 +27,8 @@ Yoshiyuki Sadamoto's Neon Genesis Evangelion manga is an analytical marvel, not 
 its quest for narrative coherence and character clarity, it demystifies some of the anime's most potent, abstract horror. It provides answers where Anno offered only questions. The result is an ending that is definitive, concrete, and deeply melancholic, but it lacks the boundary-pushing, fourth-wall-shattering audacity of The End of Evangelion.
 """) # returns json in python looks like dictionary
 
-print(result)
+
 print(result['summary'])
-print(result['sentiment'])
+print(f"Sentiment: "+result['sentiment']) 
+print(result['pros'])
+print(result['cons'])
